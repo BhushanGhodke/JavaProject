2b2dc0cae58917943aa9b957bd97e0fad4fd2bca
BhushanGhodke <bhushanghodke1500@gmail.com>
2022-12-28 00:57:14 +0530

    arraylist added in javaproject

---
+++ src/com/excellon/soft/ArrayListDemo.java
@@ -0,0 +1,15 @@
+package com.excellon.soft;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<>();
+		list.add("Bhushan");
+		list.add("Ghodke");
+		list.add("Ahmednagar");
+		System.out.println(list);
+	}
+}
