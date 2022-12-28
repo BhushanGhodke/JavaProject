5248d2727c77713ebc7c09646a8fad226141e2c9
BhushanGhodke <bhushanghodke1500@gmail.com>
2022-12-28 17:32:04 +0530

    LinkedList added to JavaProject

---
+++ src/com/excellon/soft/LinkedListDemo.java
@@ -0,0 +1,15 @@
+package com.excellon.soft;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+
+	public static void main(String[]args) {
+		List<String> list = new LinkedList<String>();
+		list.add("Bhushan");
+		list.add("Ghodke");
+		list.add("Ahmednagar");
+		System.out.println(list);
+	}
+}
