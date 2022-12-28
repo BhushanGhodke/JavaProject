18f6c4f3a85c0f29a7caab3bb6dcb18b3d5ae9e6
BhushanGhodke <bhushanghodke1500@gmail.com>
2022-12-28 23:15:44 +0530

    hashset demo added

---
+++ src/com/excellon/soft/HashSetDemo01.java
@@ -0,0 +1,18 @@
+package com.excellon.soft;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetDemo01 {
+
+	public static void main(String[] args) {
+		
+		Set<Integer> set = new HashSet<>();
+		set.add(100);
+		set.add(102);
+		set.add(1050);
+	
+		set.stream().forEach(a->System.out.println(a));
+		
+	}
+}
